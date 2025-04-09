5972066d39407edca365f5968f4747f3a487b5cc
Sumitsalunkhe777 <sumitsalunkhe7489@gmail.com>
2025-04-09 16:56:33 +0530

    navbar is back

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// main.js
+import './styles/index.css';
+import './styles/navstyle.css';
+import { renderNav } from './components/nav.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderNav();
+  });
+  
